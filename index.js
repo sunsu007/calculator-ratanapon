35e851ae1d3d24ae7e1efe82534989ef972e9242
Ratanapon Srisuneton [TH] <RSrisuneton@kerrylogistics.com>
2019-07-25 12:03:25 +0700

    update test console

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ module.exports = {
     subtract: (fr , se ) => fr - se,
     mutiply : (fr ,se ) => fr * se,
     mod : (fr , se) => fr % se,
-    divine : ( fr , se) => fr / ser
+    divine : ( fr , se) => fr / se
 }

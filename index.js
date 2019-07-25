3db730d8c410a84d63fa5eedfe8487c807b48e42
Ratanapon Srisuneton [TH] <RSrisuneton@kerrylogistics.com>
2019-07-25 10:35:22 +0700

    change index include mod

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
     plus: (fr , se) =>  fr + se,
     subtract: (fr , se ) => fr - se,
-    mutiply : (fr ,se ) => fr * se
+    mutiply : (fr ,se ) => fr * se,
+    mod : (fr , se) => fr / se; 
 }
